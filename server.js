8a605f4a9d6ad7812d8a097db0226ff216d74b73
Matthew Tobin <tobin.matthew94@gmail.com>
2023-01-05 23:52:37 -0800

    moved files to root folder

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
@@ -36,7 +36,7 @@ app.post('/api/notes', (req, res) => {
 // })
 
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
