97a4067eaf40195f61d48b40d7bf004a267e0434
Matthew Tobin <tobin.matthew94@gmail.com>
2023-01-10 12:45:33 -0800

    added comments to code

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+//require the npm packages needed to run the backend
 const express = require('express');
 const path = require('path');
+//package used to create a unique ID for each note created
 const ShortUniqueId = require('short-unique-id');
+//destructure the notes json object to use later on
 const { notes } = require('./db/db.json');
 
 const app = express();
@@ -11,14 +14,17 @@ const uid = new ShortUniqueId();
 app.use(express.json());
 app.use(express.static('public'));
 
+//HTML route to take the user to the page with all the notes
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+//API route to read the notes to the web page
 app.get('/api/notes', (req, res) => {
     res.json(notes)
 });
 
+//API route to create new notes and add to the notes object
 app.post('/api/notes', (req, res) => {
     const bodyObj = {
         "id": uid(),
@@ -35,6 +41,7 @@ app.post('/api/notes', (req, res) => {
 //     //return new notes
 // })
 
+//HTML route to the home page
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
